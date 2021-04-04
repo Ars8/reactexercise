92a668f3582e579fa1b61849dc9a2c06c54b68f7
Arseniy <pogosov-91@mail.ru>
2021-04-04 18:07:39 +0300

    AddList component

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import List from '../List';
+import Badge from '../Badge';
+
+import './AddList.scss';
+
+const AddList = ({ colors }) => {
+  const [visiblePopup, setVisiblePopup] = React.useState(false);
+  return (
+    <div className="add-list">
+      <List
+        onClick={() => setVisiblePopup(!visiblePopup)}
+        items={[
+          {
+            className: 'list__add-button',
+            icon: (
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M8 1V15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1 8H15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ),
+            name: 'Add list',
+          },
+        ]}
+      />
+      {visiblePopup && (
+        <div className="add-list__popup">
+          <img
+            onClick={onClose}
+            src={closeSvg}
+            alt="Close button"
+            className="add-list__popup-close-btn"
+          />
+
+          <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="field"
+            type="text"
+            placeholder="Название списка"
+          />
+
+          <div className="add-list__popup-colors">
+            {colors.map((color) => (
+              <Badge
+                onClick={() => selectColor(color.id)}
+                key={color.id}
+                color={color.name}
+                className={seletedColor === color.id && 'active'}
+              />
+            ))}
+          </div>
+          <button onClick={addList} className="button">
+            {isLoading ? 'Добавление...' : 'Добавить'}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddList;
